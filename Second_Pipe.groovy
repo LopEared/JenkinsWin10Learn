32b3955e0bbf783c2520190e593d33c31468d185
LopEared <chekist.moisha@gmail.com>
2022-04-14 20:59:24 +0300

    First commit

--- Second_Pipe.groovy
+++ Second_Pipe.groovy
@@ -1,61 +1,54 @@
 #!/usr/bin/groovy
 
-pipeline {
-    agent any
-	options{
-		timestamps()
-	}
-	
-    environment {
-        PROJECT_NAME 		= "Neptun"
-	    OWNER_NAME   		= "Denis Astahov"
-		ReportRepository 	= 'https://code.waters.com/bitbucket/scm/infnsd/patch-testing-results.git'
-		BUILD_NUMBER        = "${env.BUILD_NUMBER}"
-		JOB_NAME            = "${env.JOB_NAME}"
-		NODE_NAME           = "${env.NODE_NAME}"
-		WORKSPACE           = "${env.WORKSPACE}"
-		ticketNumber 		= "INFLMS26356"
-		appName				= "NG 8"
-		ReportBranchName  	= "master"  
+import java.util.concurrent.TimeoutException
+import org.jenkinsci.plugins.workflow.steps.FlowInterruptedException
+import hudson.model.Slave.*
+import hudson.slaves.OfflineCause.SimpleOfflineCause
+import hudson.slaves.OfflineCause
+
+properties([
+    parameters([
+        
+        string(name: 'ReportRepository', defaultValue: 'https://code.waters.com/bitbucket/scm/infnsd/patch-testing-results.git', description: 'Repository to store final reports', ),
+        string(name: 'ReportBranchName', defaultValue: 'master', description: 'Repository branch to store final reports', ),
+        string(name: 'appName', defaultValue: 'NG 8', description: 'Application name', ),
+        string(name: 'appVersion', defaultValue: '8', description: 'Application version', ),
+        string(name: 'ticketNumber', defaultValue: 'INFLMS-26356', description: 'Ticket number under which the commit will be pushed into Repo', ),
+    ])
+])
+
+
+def mainPipe() {
+        stage("Collecting Report info") {
+
+            def date = (new Date()).format("dd.MM.YYYY")
+            commitMsg = "${ticketNumber} reporting SDMS PatchTesting NG 8 ${date} Build #${BUILD_NUMBER}"
+
+            echo powershell(returnStdout: true, script:"""
+
+            git clone -b "${ReportBranchName}" "${ReportRepository}"
+
+            cd patch-testing-results
+
+            if(!(Test-Path "${appName}")) {
+                md "${appName}"
+            }
+
+            md "${appName}\\${date}\\${BUILD_NUMBER}"
+            dir
+            """)
+        }
+             
     }
 
-    stages {
-        stage('1-Build') {
-            steps {
-                echo "Start of Stage Build..."
-				echo "Building......."
-				echo "End of Stage Build..."
-            }
-        }
-        stage('2-Test') {
-            steps {
-                echo "Start of Stage Test..."
-				echo "Testing......."
-				echo "Privet ${PROJECT_NAME}"
-				echo "Owner is ${OWNER_NAME}"
-				echo "Repositoryy way is ${ReportRepository}"
-				
-				echo "<-------START OF MY CODE------>"
-				
-	
-				
-				echo powershell(returnStdout: true, script:"""
-
-				md ${ReportRepository}
-				cd ${ReportRepository}
-				New-Item -Path . -Name "${ReportBranchName}.txt" -ItemType "file" -Value "${ReportRepository}"
-				dir
-				$Output = Get-Content "${ReportBranchName}.txt"
-				Write-Output  $Output 
-				""")
-
-            }
-        }
-
-        stage('4-Celebrate') {
-            steps {
-                echo "CONGRATULYACIYA!"
-            }
-        }	
-	}
+timestamps {
+    try {
+        mainPipe()
+    }
+    catch(FlowInterruptedException interruptEx){
+      throw interruptEx
+    }
+    finally {
+        echo "THis is the END"
+    }
 }
