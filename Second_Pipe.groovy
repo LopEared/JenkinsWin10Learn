7e3e6de6860328b58f19db5a464dde21cedab86a
LopEared <chekist.moisha@gmail.com>
2022-04-14 16:02:08 +0300

    First commit

--- Second_Pipe.groovy
+++ Second_Pipe.groovy
@@ -1,8 +1,17 @@
 pipeline {
     agent any
+	options{
+		timestamps()
+	}
+	
     environment {
-      PROJECT_NAME = "Neptun"
+        PROJECT_NAME = "Neptun"
 	    OWNER_NAME   = "Denis Astahov"
+		repository_link = 'https://code.waters.com/bitbucket/scm/infnsd/patch-testing-results.git'
+		BUILD_NUMBER        = "${env.BUILD_NUMBER}"
+		JOB_NAME            = "${env.JOB_NAME}"
+		NODE_NAME           = "${env.NODE_NAME}"
+		WORKSPACE           = "${env.WORKSPACE}"
     }
 
     stages {
@@ -19,16 +28,11 @@ pipeline {
 				echo "Testing......."
 				echo "Privet ${PROJECT_NAME}"
 				echo "Owner is ${OWNER_NAME}"
+				echo "Repositoryy way is ${repository_link}"
 				echo "End of Stage Build..."
             }
         }
-        stage('3-Deploy') {
-            steps {
-                echo "Start of Stage Deploy..."
-				echo "Deploying......."
-				echo "End of Stage Build..."
-            }
-        }
+
         stage('4-Celebrate') {
             steps {
                 echo "CONGRATULYACIYA!"
