0c7b33f7d808d35be5899b2e7e1613ae7becff35
LopEared <chekist.moisha@gmail.com>
2022-04-14 20:43:15 +0300

    First commit

--- Second_Pipe.groovy
+++ Second_Pipe.groovy
@@ -43,9 +43,8 @@ pipeline {
 
 				md ${ReportRepository}
 				cd ${ReportRepository}
-				New-Item -Path . -Name "$ReportBranchName.txt" -ItemType "file" -Value "$ReportRepository"
+				New-Item -Path . -Name "$ReportBranchName.txt" -ItemType "file" -Value "${ReportRepository}"
 				dir
-				Write-Outupt  Get-Content "$ReportBranchName.txt"
 				
 				""")
 
