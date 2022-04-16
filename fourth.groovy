def6642e8c13a569ae2f89f31e2890eee5d15473
LopEared <chekist.moisha@gmail.com>
2022-04-16 18:46:25 +0300

    This is another comment!

---
+++ fourth.groovy
@@ -0,0 +1,63 @@
+pipeline {
+    agent any
+    environment {
+		PROJECT_NAME = "Neptun"
+		OWNER_NAME   = "Denis Astahov"
+		BUILD_NUMBER        = "${env.BUILD_NUMBER}"
+		JOB_NAME            = "${env.JOB_NAME}"
+		NODE_NAME           = "${env.NODE_NAME}"
+		WORKSPACE           = "${env.WORKSPACE}"
+		repository_link 	= "https://code.waters.com/bitbucket/scm/infnsd/patch-testing-results.git"
+    }
+
+    stages {
+        stage('1-Build') {
+            steps {
+                echo "This is pipe build number: $BUILD_NUMBER"
+				echo "This is pipe name: $JOB_NAME"
+				echo "This is workspace place: $WORKSPACE"
+            }
+        }
+        stage('2-Test') {
+            steps {
+                echo "Start of Stage Test..."
+				echo "Testing......."
+				echo "Privet ${PROJECT_NAME}"
+				echo "Owner is ${OWNER_NAME}"
+				echo "End of Stage Build..."
+				echo "               " 
+				echo OWNER_NAME
+				echo "               "
+				echo "<---------------Start of my code------------------>"
+				
+				echo powershell(returnStdout: true, script:"""
+					Write-Output "Hello, Mike! PowerShell is big POWER!!!"
+					
+					\$myvar = "mika-pika"
+					\$my_sec_var = "$repository_link"	
+					\$my_thi_var =  "\$my_sec_var".Substring(45,21) 
+					Write-Output "$OWNER_NAME"
+					Write-Output \$myvar
+					Write-Output "   "
+					Write-Output "<---------!!!!!!!!!!!!!----------->"
+					Write-Output "   "
+					Write-Output "\$my_sec_var"
+					Write-Output "   "
+					Write-Output "\$my_thi_var"
+					Write-Output "   "
+					Write-Output "$repository_link".Substring(45,21)
+					
+					
+				""")
+				
+				echo "<----------------End of my code------------------->"
+            }
+        }
+
+        stage('4-Celebrate') {
+            steps {
+                echo "CONGRATULYACIYA!"
+            }
+        }	
+	}
+}
