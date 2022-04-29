b5f54f27901ab1773198dadd95699ab7347b4faa
LopEared <40773335+LopEared@users.noreply.github.com>
2022-04-29 15:21:46 +0300

    Update Fifth_Pipe.groovy
    25

--- Fifth_Pipe.groovy
+++ Fifth_Pipe.groovy
@@ -60,6 +60,7 @@ def specificDate() {
                 md "${appName}\\${repoFolder}\\${monthYear}\\${date}\\${BUILD_NUMBER}"
 		cd "${appName}\\${repoFolder}\\${monthYear}\\${date}\\${BUILD_NUMBER}"
 		pwd
+		Copy-Item -Path "C:\\test_file.txt" -Destination "C:\\mike_builder\\Git\\${appName}\\${repoFolder}\\${monthYear}\\${date}\\${BUILD_NUMBER}"
 		""")      
  
             }
