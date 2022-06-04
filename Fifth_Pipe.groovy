9c43bb8ddc83310077987ff74c25c947eab5dc45
LopEared <40773335+LopEared@users.noreply.github.com>
2022-06-04 22:10:59 +0300

    Update Fifth_Pipe.groovy

--- Fifth_Pipe.groovy
+++ Fifth_Pipe.groovy
@@ -41,24 +41,9 @@ def CheckPrinter() {
 	stage('Check printer settings'){
 		node("master"){
 			echo powershell(returnStdout: true, script:"""
-				echo "Start powershell"
-				echo "Disable Windows smartScreen scan"
-				new-itemproperty "HKLM:\\Software\\Policies\\Microsoft\\Windows\\System" -Name EnableSmartScreen -Value 0 -Type DWORD -Force
-				new-itemproperty "HKLM:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\" -Name SmartScreenEnabled -Value Off -Type String -Force
-				echo "Disable windows Firewall"
-				Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-				echo "Disable Windows Defender "
-				Set-MpPreference -DisableRealtimeMonitoring \$true
-				echo "Set print NuGenesis UNIFY 8.0 as Default printer"
-				\$MYPRINTER = "NuGenesis UNIFY 8.0" 
-				\$PRINTERTMP = (Get-CimInstance -ClassName CIM_Printer | WHERE {\$_.Name -eq \$MYPRINTER}[0])
-				\$PRINTERTMP | Invoke-CimMethod -MethodName SetDefaultPrinter | Out-Host
-				echo "<---------------------------------------------------------->"
-				echo "Check Default printer:"
-				echo "<---------------------------------------------------------->"
-				Get-CimInstance -ClassName CIM_Printer | WHERE {\$_.Default -eq \$True} | Select Name
-				
-				
+			echo "Check Default printer:"
+			\$DefPrinter = (Get-CimInstance -ClassName CIM_Printer | WHERE {\$_.Default -eq \$True} | Select Name)
+			echo "\$DefPrinter"
 			""")
 		}
 	}
